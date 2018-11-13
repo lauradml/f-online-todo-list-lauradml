57f5b9a2b1845f8a8861ee3f747f54aedb9907a0
Laura Domingo <laura.domingo.82@gmail.com>
2018-11-13 04:00:52 -0800

    añadir y quitar ventana modal

--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@
 const list = document.querySelector('.list');
 const addButton = document.querySelector('.add-button');
 const inputTask = document.querySelector('.imput-task');
+const modal = document.querySelector('.modal');
+const plus = document.querySelector('.container-plus');
+
+
 let tasks = [];
 
 let newTask = {
@@ -15,8 +19,7 @@ let newTask = {
 function printTasks(){
     list.innerHTML = '';
     if(JSON.parse(localStorage.getItem('tasks'))){
-      tasks = JSON.parse(localStorage.getItem('tasks'));
-
+      tasks = JSON.parse(localStorage.getItem('tasks'))
         for(let i = 0; i < tasks.length; i++){
             const newTask = document.createElement('li');
             newTask.classList.add('tasks-item');
@@ -29,12 +32,11 @@ function printTasks(){
             taskInput.type = 'checkbox';
             taskInput.setAttribute('name', 'tasks');
             taskInput.setAttribute('id', tasks[i].order);
-
+            // taskInput.addEventListener('change', positionTasks);
             if(tasks[i].checked === true){
-                newTask.classList.add('.cross-out');
+                newTask.classList.add('cross-out');
                 taskInput.checked = true;
             }
-
             newTask.appendChild(taskInput);
             newTask.appendChild(taskLabel);
             list.appendChild(newTask);
@@ -44,24 +46,58 @@ function printTasks(){
 
 printTasks();
 
-
 function writeTasks(e){
     newTask.value = e.currentTarget.value;
 }
 
+function reorder(){
+    tasks.map((item, order) => {
+        return item.order = order;
+    });
+}
 
 function saveLocalStorage(){
     localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+modal.classList.add('closed');
+function toggle() {
+  if (modal.classList.contains('closed')) {
+    modal.classList.remove('closed');
+    plus.classList.add('closed')
+  } else {
+    modal.classList.add('closed');
+    plus.classList.remove('closed')
+  }
 }
 
 function functionsTasks(){
     tasks.unshift(newTask);
     saveLocalStorage();
     printTasks();
-
+    reorder();
+    toggle()
 }
 
-
+// function positionTasks(e){
+//     const id = e.target.id;
+//     const item = tasks[id];
+//     if(item.checked === false){
+//         const possition = tasks.length;
+//         item.checked = true;
+//         item.insertAdjacentHTML('beforebegin',possition);
+//         reorder();
+//         saveLocalStorage();
+//         printTasks();
+//     } else {
+//         item.checked = false;
+//         tasks.
+//         reorder();
+//         saveLocalStorage();
+//         printTasks();
+//     }
+// }
 
 addButton.addEventListener('click', functionsTasks);
 inputTask.addEventListener('change', writeTasks);
+plus.addEventListener('click', toggle);
